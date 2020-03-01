75c3cdca545d0ccc614d9760f5c22e16ca0685a9
stasisha <stasisha@users.noreply.github.com>
2020-03-01 13:10:47 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
-# Linux post install wrapper
-#
-# Currently Supported Operating Systems:
-#
-# Ubuntu
-# RHEL/CentOS
-# Debian
+# Ubuntu post install wrapper
 
 # Am I root?
 if [ "x$(id -u)" != 'x0' ]; then
     echo 'Error: this script can only be executed by root'
     exit 1
 fi
-
-# Detect OS
-case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
-    Debian)
-        type="debian"
-        pm="apt-get"
-        ;;
-    Ubuntu)
-        type="ubuntu"
-        pm="apt-get"
-        ;;
-    *)
-        type="rhel"
-        pm="yum"
-        ;;
-esac
 
 read -p 'Would you like to upgrade system? [y/n]: ' upgrade
 read -p 'Would you like to install Play On Linux? [y/n]: ' pol
@@ -41,29 +19,9 @@ read -p 'Would you like to install Notepadqq? [y/n]: ' notepadqq
 read -p 'Would you like to install Filezilla? [y/n]: ' filezilla
 read -p 'Would you like to install Vesta? [y/n]: ' vesta
 
-case $type in
-    debian)
-        # Debian special commands
-        echo "deb http://http.us.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-        apt-get update
-        ;;
-    ubuntu)
-        # Ubuntu special commands
-        ;;
-    rhel)
-        # RHEL/CentOS special commands
-        dhclient
-        yum update
-        ;;
-esac
+pm="apt-get"
 
 install="${pm} -y install"
-
-# Vesta
-if [ "$vesta" == 'y' ] || [ "$vesta" == 'Y'  ]; then
-    curl -O https://raw.githubusercontent.com/stasisha/vesta/master/install/vst-install.sh
-    bash vst-install.sh
-fi
 
 # Upgrade
 if [ "$upgrade" == 'y' ] || [ "$upgrade" == 'Y'  ]; then
@@ -109,49 +67,15 @@ fi
 
 # PhpStorm
 if [ "$phpstorm" == 'y' ] || [ "$phpstorm" == 'Y'  ]; then
-
-    case $(type) in
-        debian)
-            wget https://download-cf.jetbrains.com/webide/PhpStorm-2018.2.2.tar.gz - O /tmp/PhpStorm.tar.gz
-            tar xvf /tmp/PhpStorm.tar.gz
-            ;;
-        ubuntu)
-            eval "snap install phpstorm --classic"
-            ;;
-        rhel)
-            type="rhel"
-            pm="yum"
-            ;;
-    esac
+    eval "${install} snapd snapd-xdg-openCopy"
+    eval "snap install phpstorm --classic"
 fi
 
 # Notepadqq
 if [ "$notepadqq" == 'y' ] || [ "$notepadqq" == 'Y'  ]; then
-    case $(type) in
-        debian)
-            #https://launchpad.net/%7Enotepadqq-team/+archive/ubuntu/notepadqq/+packages
-        
-            #amd64
-            https://launchpad.net/~notepadqq-team/+archive/ubuntu/notepadqq/+build/13579637
-            #i386
-            https://launchpad.net/~notepadqq-team/+archive/ubuntu/notepadqq/+build/13579638
-            
-            #common
-            https://launchpad.net/~notepadqq-team/+archive/ubuntu/notepadqq/+files/notepadqq-common_1.2.0-1~zesty1_all.deb
-            
-            dpkg -i notepadqq-common_1.2.0-1~zesty1_all.deb 
-            dpkg -i notepadqq_1.2.0-1~zesty1_i386.deb
-            ;;
-        ubuntu)
-            add-apt-repository ppa:notepadqq-team/notepadqq -y
-            eval "${pm} update"
-            eval "${install} notepadqq"
-            ;;
-        rhel)
-            type="rhel"
-            pm="yum"
-            ;;
-    esac
+    add-apt-repository ppa:notepadqq-team/notepadqq -y
+    eval "${pm} update"
+    eval "${install} notepadqq"
 fi
 
 # FileZilla
